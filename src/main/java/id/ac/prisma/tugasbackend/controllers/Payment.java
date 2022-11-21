80222e5531685dea437cf076df84c1822f74cf5b
Fruitsallad <tombokanlester@gmail.com>
2022-11-21 09:17:59 +0800

    user service

--- src/main/java/id/ac/prisma/tugasbackend/controllers/Payment.java
+++ src/main/java/id/ac/prisma/tugasbackend/controllers/Payment.java
@@ -1,10 +1,11 @@
 package id.ac.prisma.tugasbackend.controllers;
 
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import id.ac.prisma.tugasbackend.data.model.TBMerchant;
+import id.ac.prisma.tugasbackend.data.repo.TBMerchantRepository;
+import id.ac.prisma.tugasbackend.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +13,39 @@ import java.util.Map;
 @RestController
 @RequestMapping(value = {"/integration"})
 public class Payment {
+
+    @Autowired
+    TBMerchantRepository tbMerchantRepository;
+
+    @Autowired
+    UserService UserService;
+
+    @RequestMapping(value = "/merchant/by-id/{id}", method = RequestMethod.GET, produces = "application/json")
+    public Map getUserById(@PathVariable("id") Integer id) {
+        Map response = new HashMap();
+        TBMerchant tbMerchant = tbMerchantRepository.findById(id).orElse(null);
+        response.put("data", tbMerchant);
+        return response;
+    }
+
+    @RequestMapping(value = "/merchant/create-merchant", method = RequestMethod.POST, produces = "application/json")
+    public Map createUser(@RequestBody Map bodyRequest) {
+        // call function createUser
+        Map createUserResponse = UserService.createUser(
+                bodyRequest.get("name").toString(),
+                bodyRequest.get("email").toString(),
+                bodyRequest.get("address").toString()
+        );
+        Map response = new HashMap();
+        response.put("message", "success");
+        response.put("data", createUserResponse.get("data"));
+        return response;
+    }
+    @RequestMapping(value = "/merchant/get-all-merchants", method = RequestMethod.GET, produces = "application/json")
+    public Iterable<TBMerchant> getAllUsers() {
+        Iterable<TBMerchant> allUser = tbMerchantRepository.findAll();
+        return allUser;
+    }
 
     @RequestMapping(value = "/submit-trx", method = RequestMethod.POST, produces = "application/json")
     public Map payment(@RequestBody Map bodyRequest) {
@@ -27,3 +61,4 @@ public class Payment {
         return response;
     }
 }
+
--- src/main/java/id/ac/prisma/tugasbackend/services/UserService.java
+++ src/main/java/id/ac/prisma/tugasbackend/services/UserService.java
@@ -1,4 +1,39 @@
 package id.ac.prisma.tugasbackend.services;
 
+import id.ac.prisma.tugasbackend.data.model.TBMerchant;
+import id.ac.prisma.tugasbackend.data.repo.TBMerchantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class UserService {
+    @Autowired
+    static
+    TBMerchantRepository tbMerchantRepository;
+
+    public static Map createUser(
+            String name,
+            String email,
+            String address
+    ){
+        TBMerchant tbMerchant = new TBMerchant();
+        tbMerchant.setName(name);
+        tbMerchant.setEmail(email);
+        tbMerchant.setAddress(address);
+        tbMerchant = tbMerchantRepository.save(tbMerchant);
+
+        Map response = new HashMap();
+        response.put("data", tbMerchant);
+        return response;
+    }
+    void createUser2(){
+
+    }
+
+
+
+
 }
